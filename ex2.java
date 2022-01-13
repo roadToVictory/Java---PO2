d63a48b3e56ffc7e019d5ebc7e2bb4403fa99a05
koperstynski <koperstynski@student.agh.edu.pl>
2022-01-13 01:15:59 +0100

    first lab

---
+++ ex2.java
@@ -0,0 +1,15 @@
+public class ex2 {
+    public static void main(String[] args) {
+
+        int n = Integer.parseInt(args[0]);
+        int min = Integer.parseInt(args[1]);
+        int max = Integer.parseInt(args[2]);
+
+        for(int i=0; i<n; i++){
+
+            //Random r = new Random();
+        }
+       
+    }
+}
+
